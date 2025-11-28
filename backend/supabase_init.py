4ad402f9c761b50c1e98951fcf013a6156a09516
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-11-28 12:13:12 -0800

    Use uv run fastapi dev main to spin up backend server and use .env file

--- backend/supabase_init.py
+++ backend/supabase_init.py
@@ -1,11 +1,12 @@
 import json
+import os
 from supabase import create_client, Client
+from dotenv import load_dotenv
 
-with open("config.json") as config_file:
-    config = json.load(config_file)
+load_dotenv()
 
-supabase_url = config["SUPABASE_URL"]
-supabase_key = config["SUPABASE_API_KEY"]
+supabase_url: str = os.getenv("SUPABASE_URL") or ""
+supabase_key: str = os.getenv("SUPABASE_API_KEY") or ""
 
 supabase: Client = create_client(supabase_url, supabase_key)
 print("Supabase initialized successfully:", supabase)
