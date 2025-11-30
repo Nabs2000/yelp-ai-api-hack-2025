0a56bd4af73ac903274ba9990348847d218f784e
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-11-29 16:58:09 -0800

    Add user types

---
+++ frontend/src/types/user.types.ts
@@ -0,0 +1,6 @@
+import { type UserMetadata } from "./userMetadata.types";
+
+export interface User {
+  id: string;
+  user_metadata: UserMetadata;
+}
---
+++ frontend/src/types/userMetadata.types.ts
@@ -0,0 +1,6 @@
+export interface UserMetadata {
+  email: string;
+  email_verified: boolean;
+  first_name: string;
+  last_name: string;
+}
